af7033d7977c432a8afc2cab395a6120fb193d77
GitHub Action <action@github.com>
2022-10-09 11:56:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/dontdoubt/mensuryplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/dontdoubt/mensuryplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.dontdoubt.mensuryplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dontdoubt.mensuryplugin.MyBundle
 
 class MyApplicationService {
 
